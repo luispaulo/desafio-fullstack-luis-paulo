967c8b90b21852ddb3b0136405407e55ec88ad85
luispaulo <luispaulolpsn@gmail.com>
2024-03-04 09:18:43 -0300

    add more model

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     protected $hidden = ['created_at', 'updated_at'];
+    
+    protected $fillable = [
+        'name','email','created_at', 'updated_at'
+    ];
 }
